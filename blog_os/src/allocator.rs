b3fba87d168d36451c6f7dbe87fd7e280d13a2ee
Gautam <gauravyadav.yaduvanshi@gmail.com>
2025-04-10 16:20:21 -0400

    Add LinkedList Allocator Implementation

--- blog_os/src/allocator.rs
+++ blog_os/src/allocator.rs
@@ -7,7 +7,9 @@ use x86_64::{
     };
 use linked_list_allocator::LockedHeap;
 pub mod bump;
+pub mod linked_list;
 use bump::BumpAllocator;
+use linked_list::LinkedListAllocator;
 
 pub struct Locked<A> {
     inner: spin::Mutex<A>,
@@ -40,8 +42,11 @@ unsafe impl GlobalAlloc for Dummy {
 //#[global_allocator]
 //static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
+//#[global_allocator]
+//static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 800*1024;
