b5b1d9dc11593997b52b0d111803a9e0a18ab903
Gautam <gauravyadav.yaduvanshi@gmail.com>
2025-04-10 15:07:33 -0400

    Heap allocation with Global allocator

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -3,6 +3,7 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+extern crate alloc;
 
 use core::panic::PanicInfo;
 use blog_os::println;
@@ -12,6 +13,7 @@ use x86_64::{
     VirtAddr,
     structures::paging::PageTable,
 };
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 
 entry_point!(kernel_main);
 
@@ -33,9 +35,10 @@ fn panic(info: &PanicInfo) -> ! {
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     
+    use blog_os::allocator;
     println!("Red Rust is {}!!", "eternal");
     blog_os::init();
-    
+  
     fn stack_overflow() {
         stack_overflow();
     }
@@ -73,7 +76,27 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = memory::EmptyFrameAllocator;
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap allocation failed");
+    let heap_value = Box::new(51);
+    println!("heap_value at {:p}", heap_value);
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
     let page = Page::containing_address(VirtAddr::new(0));
     memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
