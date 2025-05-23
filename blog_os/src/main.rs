7dc852c90a4b1d34cfaa5df77897fb10e64b99b0
Gautam <gauravyadav.yaduvanshi@gmail.com>
2025-05-23 16:53:04 -0400

    Async Await ->  Executor

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -14,6 +14,8 @@ use x86_64::{
     structures::paging::PageTable,
 };
 use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+use blog_os::task::{Task, simple_executor::SimpleExecutor, keyboard};
+use blog_os::task::executor::Executor;
 
 entry_point!(kernel_main);
 
@@ -32,6 +34,15 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 // static HELLO: &[u8] = b"Red rust is a mood!";
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
+}
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     
@@ -85,11 +96,11 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let heap_value = Box::new(51);
     println!("heap_value at {:p}", heap_value);
     // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
+    //let mut vec = Vec::new();
+    //for i in 0..500 {
+    //    vec.push(i);
+    //}
+    //println!("vec at {:p}", vec.as_slice());
 
     // create a reference counted vector -> will be freed when count reaches 0
     let reference_counted = Rc::new(vec![1, 2, 3]);
@@ -131,14 +142,18 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         // virtual address mapped to physical address 0
         boot_info.physical_memory_offset,
     ];
-
+    // let mut executor = SimpleExecutor::new();
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 //    for &address in &addresses {
 //        let virt = VirtAddr::new(address);
 //        // let phys = unsafe { translate_addr(virt, phys_mem_offset) };
 //        let phys = mapper.translate_addr(virt);
 //        println!("{:?} -> {:?}", virt, phys);
 //    }
-
+    
     println!("I am speed!");
     //loop {
     //    use blog_os::print;
